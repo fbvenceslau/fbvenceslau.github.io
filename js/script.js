cf8b93891cc0667905405c6248166f73625a8c11
Fabio Venceslau <fb.venceslau@gmail.com>
2024-09-01 23:42:12 -0300

    Index concluído

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Exemplo de script simples para rolagem suave
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
       e.preventDefault();
